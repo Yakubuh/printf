25ca7a0d8819fb259a82c87e13a2b7ca1ffe08ce
Bayulu Yakubu <105476695+Yakubuh@users.noreply.github.com>
2022-10-17 21:37:03 +0000

    Update functions2.c

--- functions2.c
+++ functions2.c
@@ -104,7 +104,6 @@ int print_non_printable(va_list types, char buffer[],
  * Return: Numbers of chars printed
  */
 
-
 int print_reverse(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
